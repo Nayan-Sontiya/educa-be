a8186d51b82943db30c401e6e675e68314b62802
nayan <sontiya.nayan@gmail.com>
2026-07-06 21:33:07 +0530

    fix: testing upload

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use("/api/test/whatsapp", require("./routes/whatsAppTestRoutes"));
 require("./cron/markTeachersAbsent");
 require("./cron/subscriptionJobs");
 
-app.get("/", (req, res) => res.send("EduVerse API is running 🚀"));
+app.get("/", (req, res) => res.send("EduVerse API is running test🚀"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🌍 Server running on port ${PORT}`));
